e6969feb43b264936e86dcf34b0fe85f9e9a0e3b
Dharmvir Patel <dharmvir786@gmail.com>
2018-05-25 16:53:25 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  
+  itemCount: number;
+  btnText: string = 'Add an Item';
+  goalText: string;
+  goals = [];
+  constructor() { }
+
+  ngOnInit() {
+  	this.itemCount = this.goals.length;
+  }
+  addItem(){
+  	this.goals.push(this.goalText);
+  	this.goalText = '';
+  	this.itemCount = this.goals.length;
+  }
+  removeItem(i){
+  	this.goals.splice(i,1);
+  	this.itemCount = this.goals.length;
+  }
+
+}
