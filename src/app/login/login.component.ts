66c1888d4f9319a2168278a0da90f163cc4f744b
Dharmvir Patel <dharmvir786@gmail.com>
2018-06-08 15:23:45 +0530

    login module work

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  isLoginError:boolean=false;
+  isLogin:boolean=false;
+  userName:string;
+  userPassword:string;
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  OnSubmit(){
+  	if(this.userName=="demo" && this.userPassword=="demo"){
+  		//localStorage.setItem('userToken','');
+        //this.router.navigate(['/home']);
+        this.isLoginError = false;
+        this.isLogin = true;
+        //this.router.navigate(['/members']);
+  	}
+  	else{
+  		this.isLogin = false;
+  		this.isLoginError = true;
+  	}
+  	
   }
 
 }
