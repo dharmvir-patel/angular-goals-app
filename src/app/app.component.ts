41124915c693f4ae303cce554ab549015ad952ac
Dharmvir Patel <dharmvir786@gmail.com>
2018-06-26 17:44:19 +0530

    contact page api change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'app';
+  loginNavText:string;
   date = new Date();
+
+  constructor(private loginService : LoginService) { }
+
+  ngOnInit() {
+  	if(this.loginService.isUserLoggedIn()){
+  		this.loginNavText = 'My Account';
+  	}
+  	else{
+  		this.loginNavText = 'Login';
+  	}
+  }
+
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-contact',
@@ -13,8 +16,8 @@ export class ContactComponent implements OnInit {
   phone = new FormControl('');
   message = new FormControl('');
   isSussess:boolean=false;
-
-  constructor() { }
+  _apiUrl:string = 'http://www.digitalcahoots.com/contactengine.php';
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
@@ -26,13 +29,17 @@ export class ContactComponent implements OnInit {
   getEmailErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' : this.email.hasError('email') ? 'Not a valid email' : '';
   }
-  contactUs(){
+  contactUs():void{
   	if(this.name.valid &&  this.email.valid ){
+      
+      this.http.post<any>(this._apiUrl,{Email:this.email.value,Name:this.name.value,Message:this.message.value,Phone:this.phone.value})
+  		.subscribe(res=>console.log(res));
+
       this.isSussess = true;
-  		console.log("Success"+this.name.value);
-      console.log("Success"+this.email.value);
-      console.log("Success"+this.phone.value);
-      console.log("Success"+this.message.value);
+      this.name.reset();
+      this.email.reset();
+      this.phone.reset();
+      this.message.reset();
 
     }
   }
